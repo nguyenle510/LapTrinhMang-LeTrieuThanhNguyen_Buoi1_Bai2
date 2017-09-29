61eeb98e1ad5c60273d9a1aa80ef8acec1d43317
nguyenle510 <trieunguyen30@gmail.com>
2017-09-29 16:18:43 +0700

    Bai2

---
+++ Bai2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap Ten Mien:");
+            String host = Console.ReadLine();
+            IPHostEntry ipHE = Dns.GetHostEntry(host);
+            Console.WriteLine("HostName: {0}", ipHE.HostName);
+
+            foreach (string s in ipHE.Aliases)
+            {
+                Console.WriteLine("\t+Alias: {0} \n", s);
+            }
+            foreach (IPAddress i in ipHE.AddressList)
+            {
+                Console.WriteLine("\t+ IP: {0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
